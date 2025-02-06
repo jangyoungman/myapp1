92e5635436e7591157cc93abc5392bc03a4ae9a5
jangyoungman <gellotin@gmail.com>
2025-02-06 11:22:06 +0900

    디테일페이지 안나오는 문제 해결

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Movie({ id, coverImg, title, summary, genres }) {
     return <div>
         <img src={coverImg} alt={title} />
-        <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+        <h2><Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link></h2>
         <p>{summary}</p>
         <ul>
             {genres.map((g) => (
